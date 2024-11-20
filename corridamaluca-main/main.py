95f5afc129edade7914564fa83f917c65d47790a
Natália <natii.machado123@gmail.com>
2024-11-20 15:30:05 -0300

    Ajustei outros códigos, modifiquei e coloquei a distancia entre os carros aparecerem na tela

--- corridamaluca-main/main.py
+++ corridamaluca-main/main.py
@@ -1,3 +1,4 @@
+#Imports e recursos
 import pygame
 import random
 pygame.init()
@@ -5,7 +6,6 @@ tamanho = (1000,592)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode( tamanho )
 icone = pygame.image.load("Recursos/icone.ico")
-
 pygame.display.set_icon(icone)
 pygame.display.set_caption("Corrida Maluca")
 branco = (255,255,255)
@@ -15,6 +15,7 @@ carroVermelho = pygame.image.load("Recursos/carro1.png")
 carroAmarelo = pygame.image.load("Recursos/carro2.png")
 carroAzul = pygame.image.load("Recursos/carro3.png")
 
+#Movimentaações dos carros e aonde vão ficar na imagem
 movXCarVermelho = 0
 movXCarAmarelo = 0
 movxcarAzul=0
@@ -22,6 +23,7 @@ posYCarVermelho = 50
 posYCarAmarelo = 100
 posycarAzul=180
 
+#Músicas 
 vitoria = pygame.mixer.Sound("Recursos/vitoria.mp3")
 vitoria.set_volume(0.5)
 pygame.mixer.music.load("Recursos/trilha.mp3")
@@ -29,7 +31,7 @@ pygame.mixer.music.play(-1)
 acabou = False
 somDaVitoria = False
 
-
+#Looping da corrida
 while True:
     for evento in pygame.event.get(): 
         if evento.type == pygame.QUIT:
@@ -45,14 +47,26 @@ while True:
         movXCarVermelho = movXCarVermelho + random.randint(0,10)
         movXCarAmarelo = movXCarAmarelo + random.randint(0,10)
         movxcarAzul=movxcarAzul+random.randint(0,10)
-
     else:
         pygame.mixer.music.stop()
         if somDaVitoria == False:
             pygame.mixer.Sound.play(vitoria)
             somDaVitoria = True
-        
-    
+
+
+    fonte = pygame.font.Font(None, 20)
+    texto = fonte.render(f"Distância Vermelho: {(movXCarAmarelo+movxcarAzul) - 1000 - movXCarVermelho}", True, (0, 0, 0),branco)
+    tela.blit(texto, (800,0)) 
+
+    fonte = pygame.font.Font(None, 20)
+    texto = fonte.render(f"Distância Amarelo: {(movXCarVermelho + movxcarAzul) - 1000 - movXCarAmarelo}", True, (0, 0, 0),branco)
+    tela.blit(texto, (800,20))   
+
+    fonte = pygame.font.Font(None, 20)
+    texto = fonte.render(f"Distância Azul: {(movXCarVermelho + movXCarAmarelo) - 1000 - movxcarAzul}", True, (0, 0, 0),branco)
+    tela.blit(texto, (800,45))   
+
+#Pisição de cada carrinho na segunda pista e movimentação
     if movXCarVermelho > 1000:
         movXCarVermelho = 0
         posYCarVermelho = 350
@@ -63,28 +77,25 @@ while True:
 
     if movxcarAzul > 1000:
         movxcarAzul = 0
-        posycarAzul = 500                    
-    
-    fonte = pygame.font.Font("freesansbold.ttf",60)
-    textoVermelho = fonte.render("Vermelho Ganhou!", True, branco)
-    textoAmarelo = fonte.render("Amarelo Ganhou!", True, branco)
-    textoAzul = fonte.render("Azul Ganhou!", True, branco)
+        posycarAzul = 500       
 
+            
+#Determina quem ganhou     
     if posYCarVermelho == 350 and movXCarVermelho >= 900 and movXCarVermelho > movXCarAmarelo and movxcarAzul:
-        tela.blit(textoVermelho, (270,70))
+        tela.blit( (270,70))
         acabou = True
         
     elif posYCarAmarelo == 400 and movXCarAmarelo >= 900 and movXCarAmarelo > movXCarVermelho and movxcarAzul:
-        tela.blit(textoAmarelo, (270,180))
+        tela.blit( (270,180))
         acabou = True
 
     elif posycarAzul == 500 and movxcarAzul >= 900 and movxcarAzul > movXCarAmarelo and movXCarVermelho:
-        tela.blit(textoAzul, (270,80))
+        tela.blit( (270,80))
         acabou = True     
           
 
     
-    
+#fechamento de jogo   
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
